0feb694fe1e31a3f4d5ae55bc4278358f673de57
Joshua Burt <joshburt@Joshuas-MBP.lan>
2022-12-14 12:53:30 -0800

    stashing changes

--- test/unit/anaconda/enterprise/mlops/sdk/ae5/test_secrets.py
+++ test/unit/anaconda/enterprise/mlops/sdk/ae5/test_secrets.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from src.anaconda.enterprise.mlops.sdk.ae5.secrets import load_ae5_user_secrets
@@ -5,9 +6,18 @@ from src.anaconda.enterprise.mlops.sdk.common.config.environment import demand_e
 
 
 class TestSecrets(unittest.TestCase):
+    def setUp(self) -> None:
+        if "MOCK_SECRET" in os.environ:
+            del os.environ["MOCK_SECRET"]
+
     def test_load_ae5_user_secrets(self):
         mock_path: str = "test/fixtures/secrets"
         load_ae5_user_secrets(secrets_path=mock_path)
+        self.assertEqual(demand_env_var(name="MOCK_SECRET"), "This is a mock secret.")
+
+    def test_load_ae5_user_secrets_with_silent_true(self):
+        mock_path: str = "test/fixtures/secrets"
+        load_ae5_user_secrets(secrets_path=mock_path, silent=True)
         self.assertEqual(demand_env_var(name="MOCK_SECRET"), "This is a mock secret.")
 
 
