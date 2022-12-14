aa836c0654e21a037fc7261c11ba74eee9069d88
Joshua Burt <joshburt@Joshuas-MBP.lan>
2022-12-14 10:54:50 -0800

    cleanup

--- test/unit/anaconda/enterprise/mlops/sdk/mlflow/test_factory.py
+++ test/unit/anaconda/enterprise/mlops/sdk/mlflow/test_factory.py
@@ -1,18 +1,16 @@
-# import unittest
-#
-# from dotenv import load_dotenv
-# from mlflow import MlflowClient
-#
-# from src.anaconda.enterprise.mlops.sdk.mlflow.factory import build_mlflow_client
-#
-#
-# class TestFactory(unittest.TestCase):
-#     def test_build_mlflow_client(self):
-#         load_dotenv(dotenv_path="env/env.local")
-#         client: MlflowClient = build_mlflow_client()
-#         self.assertIsInstance(obj=client, cls=MlflowClient)
-#
-#
-# if __name__ == "__main__":
-#     runner = unittest.TextTestRunner()
-#     runner.run(TestFactory())
+import unittest
+
+from mlflow import MlflowClient
+
+from src.anaconda.enterprise.mlops.sdk.mlflow.factory import build_mlflow_client
+
+
+class TestFactory(unittest.TestCase):
+    def test_build_mlflow_client(self):
+        client: MlflowClient = build_mlflow_client()
+        self.assertIsInstance(obj=client, cls=MlflowClient)
+
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner()
+    runner.run(TestFactory())
