d4481b56059e3480364ad110018c64c0cfa77386
Josh Burt <joshburt@users.noreply.github.com>
2022-12-14 16:48:24 -0800

    generate coverage report during pipeline exeuction for tracking (#3)
    Co-authored-by: Joshua Burt <joshburt@Joshuas-MBP.lan>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="anaconda.enterprise.mlops.sdk",
-    version="0.1.1",
+    version="0.1.2",
     package_dir={"": "src"},
     packages=setuptools.find_namespace_packages(where="src"),
     author="Joshua C. Burt",
