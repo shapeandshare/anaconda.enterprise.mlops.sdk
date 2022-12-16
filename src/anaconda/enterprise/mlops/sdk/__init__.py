f65272238f8bc356915e2b8a00872ad0d044f4cd
Joshua Burt <joshburt@Joshuas-MBP.lan>
2022-12-15 17:45:54 -0800

    linting support

--- src/anaconda/enterprise/mlops/sdk/__init__.py
+++ src/anaconda/enterprise/mlops/sdk/__init__.py
@@ -1,5 +1,13 @@
+""" anaconda.enterprise.mlops.sdk namespace """
+
 from .ae5.secrets import load_ae5_user_secrets
-from .common.config.environment import demand_env_var, get_env_var, demand_env_var_as_int, demand_env_var_as_float, demand_env_var_as_bool
+from .common.config.environment import (
+    demand_env_var,
+    demand_env_var_as_bool,
+    demand_env_var_as_float,
+    demand_env_var_as_int,
+    get_env_var,
+)
 from .common.config.environment_variable_not_found_error import EnvironmentVariableNotFoundError
 from .common.dto.base_model import BaseModel
 from .mlflow.factory import build_mlflow_client
--- src/anaconda/enterprise/mlops/sdk/common/dto/base_model.py
+++ src/anaconda/enterprise/mlops/sdk/common/dto/base_model.py
@@ -1,5 +1,6 @@
 """ Base Model (Pydantic) Over-Ride """
 
+# pylint: disable=no-name-in-module,too-few-public-methods
 from pydantic import BaseModel as PydanticBaseModel
 
 
